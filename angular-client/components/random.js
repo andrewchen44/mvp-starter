5d7ae96c11e06a0162cfd6593ce339111ddad43d
Andrew Chen <andrewchen.c44@gmail.com>
2017-11-11 17:22:19 -0800

    In process of rendering data

--- angular-client/components/random.js
+++ angular-client/components/random.js
@@ -1,14 +1,17 @@
 angular.module('app')
 .component('randomAdventure', {
   controller: function($http) {
-    this.adventure;
+    this.adventure =[];
 
-    this.random = function(){
-      $http.get('/random', this).then(function(responce, error){
+
+    this.random = function() {
+      var adventure = this.adventure;
+      $http.get("/random").then(function(responce, error) {
         var number = Math.floor(Math.random() * responce.data.length) + 0;
-        this.adventure = responce.data[number];
+        adventure = responce.data[number];
       });
-    }
+    };
+
   },
   templateUrl: "/templates/random.html"
 });
--- angular-client/components/render.js
+++ angular-client/components/render.js
@@ -1,6 +1,10 @@
 angular.module('app')
 .component('renderAdventure', {
+  bindings: {
+    adventure: '<'
+  },
   controller: function() {
+    
 
   },
   templateUrl: "/templates/render.html"
