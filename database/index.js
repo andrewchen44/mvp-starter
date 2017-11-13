56ce5cbe24bdd84f0964f8eb29eaf0e57ae68889
Andrew Chen <andrewchen.c44@gmail.com>
2017-11-12 19:44:58 -0800

    modified add adventure css

--- database/index.js
+++ database/index.js
@@ -41,7 +41,6 @@ var randomAdventure = function() {
 }
 
 var like = function(name, value) {
-  console.log(name, value);
   Adventure.update({Name: 'boxing'}, {Likes: value}, {safe: true}, function(err, raw) {
     if (err) return handleError(err);
     console.log('the raw responce from mongo was', raw);
